4bed149ed36ef8da17cd5e1a63fccb33f98e5238
Luccas Fialho <luccas.flh@gmail.com>
2023-04-21 17:41:05 -0300

    Refatorado then para async e await

--- controller/atualizaCliente-controller.js
+++ controller/atualizaCliente-controller.js
@@ -1,26 +1,24 @@
 import { clienteService } from "../service/cliente-service.js"
 
-const pegaURL = new URL(window.location)
+(async() => {
+    const pegaURL = new URL(window.location)
 
-const id = pegaURL.searchParams.get('id')
-
-const inputNome = document.querySelector('[data-nome]')
-const inputEmail = document.querySelector('[data-email]')
-
-clienteService.detalhaCliente(id)
-.then(dados => {
-    inputNome.value = dados.nome
-    inputEmail.value = dados.email    
-})
-
-const formulario = document.querySelector('[data-form]')
-
-formulario.addEventListener('submit', (evento) => {
+    const id = pegaURL.searchParams.get('id')
     
-    evento.preventDefault()
-
-    clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
-    .then(() => {
+    const inputNome = document.querySelector('[data-nome]')
+    const inputEmail = document.querySelector('[data-email]')
+    
+    const dados = await clienteService.detalhaCliente(id)
+    inputNome.value = dados.nome
+    inputEmail.value = dados.email       
+    
+    const formulario = document.querySelector('[data-form]')
+    
+    formulario.addEventListener('submit', async (evento) => {
+        
+        evento.preventDefault()
+    
+        await clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
         window.location.href = '../telas/edicao_concluida.html '
     })
-})
+})()
